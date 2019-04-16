ea5897a6c2fd36c255cfee540d47eea0885edda6
kk <kk@192.168.1.154>
2019-04-16 22:45:35 +0530

    controller changes

--- src/main/java/com/springbooot/model/Student.java
+++ src/main/java/com/springbooot/model/Student.java
@@ -1,12 +1,15 @@
 package com.springbooot.model;
 
+import javax.annotation.Generated;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 
 @Entity
 public class Student {
 	
 	@Id
+	@NotNull
 	private int id;
 	
 	private String name;
--- src/main/java/com/springbooot/restcontroller/StudentController.java
+++ src/main/java/com/springbooot/restcontroller/StudentController.java
@@ -1,30 +1,54 @@
 package com.springbooot.restcontroller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springbooot.model.Student;
 import com.springbooot.service.StudentRepository;
 
 @RestController
+@RequestMapping("/students")
 public class StudentController {
-	
+
 	@Autowired
 	StudentRepository studentRepository;
-	
+
 	@PostMapping("/saveStudent")
 	public Student saveStudent(@RequestBody Student student) {
-		
+
 		return studentRepository.save(student);
 	}
-	
+
 	@GetMapping("/getStudent")
 	public Student getStudent(int id) {
-		
+
 		return studentRepository.getOne(id);
+	}
+
+	@PutMapping("/updateStudent")
+	public Student updateStudent(@RequestBody Student student) {
+
+		return studentRepository.save(student);
+	}
+
+	@DeleteMapping("/deleteStudent")
+	public void deleteStudent(Integer id) {
+
+		studentRepository.deleteById(id);
+	}
+	
+	@GetMapping("/allStudents")
+	public List<Student> getStudents(){
+		
+		return studentRepository.findAll();
 	}
 
 }
